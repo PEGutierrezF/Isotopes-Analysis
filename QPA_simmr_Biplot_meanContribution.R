db971914884c0e04dc665b7c3eea6c394290fd97
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-03-12 20:41:21 -0600

    Frebruary 2017, checked

--- QPA_simmr_Biplot_meanContribution.R
+++ QPA_simmr_Biplot_meanContribution.R
@@ -2,15 +2,15 @@
 #### https://cran.r-project.org/web/packages/simmr/vignettes/simmr.html
 
 
-install.packages("readxl")
-install.packages("backports")
 library("simmr")
 library("readxl")
 library("backports")
 
-setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis")
+setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis/SIMMR QPA")
 
-##### QPA February 2017 #####
+
+# QPA February 2017  ------------------------------------------------------
+
 
 targets = read_excel("QPAFeb17.xlsx",1)
 sources = read_excel("QPAFeb17.xlsx",2)
@@ -27,10 +27,13 @@ QPAFeb17Biplot
 plot(QPAFeb17Biplot, group = 1:12)
 
 QPAFeb17_out = simmr_mcmc(QPAFeb17Biplot)
-summary(QPAFeb17_out,type='statistic', group = c(1:11))
-summary(QPAFeb17_out,type='quantiles', group = c(1:11))
+summary(QPAFeb17_out,type='statistic', group = c(1:10))
+summary(QPAFeb17_out,type='quantiles', group = c(1:10))
 
-##### QPA November 2017 #####
+
+
+
+# QPA November 2017 -------------------------------------------------------
 
 
 targets = read_excel("QPANov17.xlsx",1)
@@ -52,7 +55,9 @@ summary(QPANOV17_out,type='statistic', group = c(1:11))
 summary(QPANOV17_out,type='quantiles', group = c(1:11))
 
 
-##### QPA June 2018 #####
+
+# QPA June 2018 -----------------------------------------------------------
+
 
 targets = read_excel("QPAJune18.xlsx",1)
 sources = read_excel("QPAJune18.xlsx",2)
@@ -72,6 +77,28 @@ QPAJune18_out = simmr_mcmc(QPAJune18Biplot)
 summary(QPAJune18_out,type='statistic', group = c(1:11))
 summary(QPAJune18_out,type='quantiles', group = c(1:11))
 
+
+
+# QPA February 2019 -------------------------------------------------------
+
+
+targets = read_excel("QPAFeb19.xlsx",1)
+sources = read_excel("QPAFeb19.xlsx",2)
+TEFs <- read_excel("QPAFeb19.xlsx",3)
+
+QPAFeb19Biplot = simmr_load(mixtures = as.matrix(targets[, 1:2]),
+                            source_names = sources$Sources,
+                            source_means = sources[,2:3],
+                            source_sds = sources[,4:5],
+                            correction_means = TEFs[,2:3],
+                            correction_sds = TEFs[,4:5],
+                            group = as.factor(paste('Group', targets$Code)))
+QPAFeb19Biplot
+plot(QPAFeb19Biplot, group = 1:11)
+
+QPAFeb19_out = simmr_mcmc(QPAFeb19Biplot)
+summary(QPAFeb19_out,type='statistic', group = c(1:11))
+summary(QPAFeb19_out,type='quantiles', group = c(1:11))
 
 
 
