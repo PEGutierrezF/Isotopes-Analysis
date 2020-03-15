537a64c558e5f334d84a6ff0313426fb30c4e855
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-03-15 13:48:13 -0600

    Permanova Edited

--- Two-Way-PERMANOVA-QPA-QPB.R
+++ Two-Way-PERMANOVA-QPA-QPB.R
@@ -5,15 +5,15 @@ library("vegan")
 require(vegan)
 library(tidyverse)
 
-setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis/PERMANOVA QPA")
-
-
+setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis/PERMANOVA")
 
 # QPA 13 Carbon --------------------------------------------------------------
 
 
 QPAPermanova=read.csv("PermanovaQPA.csv")
 QPAPermanova
+
+####
 
 CarbonQPA <- select(QPAPermanova, QPACarbon)
 CarbonQPA
@@ -43,69 +43,44 @@ adonis2(NitrogenQPA1~Source*Time, data = Factors)
 
 
 
+# QPB 13 Carbon -----------------------------------------------------------
 
-################################################################
+QPBPermanova=read.csv("PermanovaQPB.csv")
+QPBPermanova
 
-setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis/PERMANOVA QPB")
+####
 
-######################### 13 C QPA ##############################
+QPBCarbon <- select(QPBPermanova, QPBCarbon)
+QPBCarbon
+QPBCarbonNA <- na.omit(QPBCarbon)
 
-CarbonQPB=read.csv("13CQPB.csv")
-CarbonQPB
+Factors <- select(QPBPermanova, Source, Time)
+Factors
+FactorsNA <- Factors[-30, ]
+FactorsNA
 
-FactorsQPB=read.csv("FactorsQPB.csv")
-FactorsQPB
-
-CarbonQPB1 <- vegdist (CarbonQPB, method = "euclidean", na.rm = T)
+CarbonQPB1 <- vegdist (QPBCarbonNA, method = "euclidean", na.rm = T)
 CarbonQPB1
 
-adonis2(CarbonQPB1 ~Source*Time, data = FactorsQPB)
+adonis2(CarbonQPB1 ~Source*Time, data = FactorsNA)
 
-######################### 15 N QPA ##############################
 
-NitrogenQPB=read.csv("15NQPB.csv")
-NitrogenQPB
 
-FactorsQPB=read.csv("FactorsQPB.csv")
-FactorsQPB
+# QPB 15 Nitrogen ---------------------------------------------------------
 
-NitrogenQPB1<- vegdist (NitrogenQPB, method = "euclidean", na.rm = T)
+QPBNitrogen <- select(QPBPermanova, QPBNitrogen)
+QPBNitrogen
+
+Factors <- select(QPBPermanova, Source, Time)
+Factors
+
+
+NitrogenQPB1<- vegdist (QPBNitrogen, method = "euclidean", na.rm = T)
 NitrogenQPB1
 
-adonis2(NitrogenQPB1~Source*Time, data = FactorsQPB)
-
-#### FIgures #####
-
-IsotopesQPA.frm=read.csv("IsotopesQPA.csv")
-attach(IsotopesQPA.frm)
-IsotopesQPA.frm
-
-library("ggpubr")
-library(ggplot2)
-ggboxplot(IsotopesQPA.frm, x = "Time", y = "C", color = "Source",width = 0.7,
-          palette = c("#00AFBB","#FC4E07"),size =1,
-          order = c("Feb", "Nov", "June"),legend = "right",
-          font.legend = c(15, "plain", "black"),
-          font.x = c(20, "bold", "black"),font.y = c(20, "bold", "black"),
-          font.xtickslab= c(15, "bold", "black"), font.ytickslab= c(15, "bold", "black"))
+adonis2(NitrogenQPB1~Source*Time, data = Factors)
 
 
-IsotopesQPB.frm=read.csv("IsotopesQPB.csv")
-attach(IsotopesQPB.frm)
-IsotopesQPB.frm
-
-
-library("ggpubr")
-library(ggplot2)
-ggboxplot(IsotopesQPB.frm, x = "Time", y = "C", color = "Source",width = 0.7,
-          palette = c("#00AFBB","#FC4E07"),size =1,
-          order = c("Feb", "Nov", "Jun"),legend = "right",
-          font.legend = c(15, "plain", "black"),
-          font.x = c(20, "bold", "black"),font.y = c(20, "bold", "black"),
-          font.xtickslab= c(15, "bold", "black"), font.ytickslab= c(15, "bold", "black"))
-
-windows()
-par(mfrow=c(2,2))
 
 
 
