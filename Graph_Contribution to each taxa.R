3766718e6e0ec0a70a67ff96c00c315fbdeb0031
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-03-20 20:22:04 -0600

    Modified IV

--- Graph_Contribution to each taxa.R
+++ Graph_Contribution to each taxa.R
@@ -5,24 +5,24 @@
 library("readxl")
 library(ggplot2)
 
-setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes_Analysis_2017_2019")
+setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis")
 
 
 # Phylloicus --------------------------------------------------------------
 
-Phylloicus = read_excel("Taxa.xlsx",1)
-Phylloicus
+PhylloicusQPA = read_excel("Graph _Source contribution.xlsx",9)
+PhylloicusQPA
 
-Phylloicus$Data <- factor(Phylloicus$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
+PhylloicusQPA$Data <- factor(PhylloicusQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
 
-g = ggplot(data=Phylloicus, aes(x=Source, y=Value, ymin=LowerCI, ymax=UpperCI, color=Data))
+g = ggplot(data=PhylloicusQPA, aes(x=Source, y=Value, ymin=LowerCI, ymax=UpperCI, color=Data))
 g = g + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
 g = g + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
 g = g + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
 g = g + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
 g = g + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
 g = g + coord_flip() + theme(legend.position="none")
-g = g + scale_colour_manual(values = c('#4dd43b', '#5c3b13')) #first Biofilm, second Leaf
+g = g + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
 g
 g1 = g +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
                     axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) # #subaxis y
