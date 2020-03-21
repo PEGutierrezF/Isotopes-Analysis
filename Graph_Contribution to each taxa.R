dc6e10d0842a8f83b47c1ade73284b2b07e32921
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-03-20 21:22:03 -0600

    Modified

--- Graph_Contribution to each taxa.R
+++ Graph_Contribution to each taxa.R
@@ -2,8 +2,11 @@
 # https://rpubs.com/nguyet/521181
 
 
-library("readxl")
+library(readxl)
 library(ggplot2)
+library(tidyverse)
+library(ggpubr)
+library(gridExtra)
 
 setwd("D:/LTER/Manuscript 2019 Stable Isotopes/Isotopes-Analysis")
 
@@ -195,31 +198,117 @@ M5
 
 # QPA Atya  ---------------------------------------------------------------
 
-XiphocarisQPA = read_excel("Graph _Source contribution.xlsx",13)
-XiphocarisQPA
+AtyaQPA = read_excel("Graph _Source contribution.xlsx",15)
+AtyaQPA
 
-XiphocarisQPA$Data <- factor(XiphocarisQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
+AtyaQPA$Data <- factor(AtyaQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
 
-M = ggplot(data=XiphocarisQPA, aes(x=Source, y=Value7A, ymin=LowerCI, ymax=UpperCI, color=Data))
-M = M + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
-M = M + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
-M = M + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
-M = M + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
-M = M + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
-M = M + coord_flip() + theme(legend.position="none")
-M = M + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
-M
-M1 = M +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
+O = ggplot(data=AtyaQPA, aes(x=Source, y=Value8A, ymin=LowerCI, ymax=UpperCI, color=Data))
+O = O + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
+O = O + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
+O = O + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
+O = O + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
+O = O + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
+O = O + coord_flip() + theme(legend.position="none")
+O = O + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
+O
+O1 = O +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
                 axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) # #subaxis y
-M2 = M1 +  theme(strip.text.y = element_text(face="bold", size=10, colour="black")) # Each title white
-M3 = M2 +  theme(panel.background = element_blank(),
+O2 = O1 +  theme(strip.text.y = element_text(face="bold", size=10, colour="black")) # Each title white
+O3 = O2 +  theme(panel.background = element_blank(),
                  panel.border = element_rect(colour = "black", fill=NA, size=1))
-M4 = M3 + theme(axis.text.y   = element_text(size=12))
-M5 = M4 + theme(axis.title.x   = element_text(size=12))
-M5
+O4 = O3 + theme(axis.text.y   = element_text(size=12))
+O5 = O4 + theme(axis.title.x   = element_text(size=12))
+O5
+
+
+# QPA Macrobrachium -------------------------------------------------------
+
+MacrobrahiumQPA = read_excel("Graph _Source contribution.xlsx",17)
+MacrobrahiumQPA
+
+MacrobrahiumQPA$Data <- factor(MacrobrahiumQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
+
+Q = ggplot(data=MacrobrahiumQPA, aes(x=Source, y=Value9A, ymin=LowerCI, ymax=UpperCI, color=Data))
+Q = Q + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
+Q = Q + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
+Q = Q + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
+Q = Q + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
+Q = Q + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
+Q = Q + coord_flip() + theme(legend.position="none")
+Q = Q + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
+Q
+Q1 = Q +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
+                axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) # #subaxis y
+Q2 = Q1 +  theme(strip.text.y = element_text(face="bold", size=10, colour="black")) # Each title white
+Q3 = Q2 +  theme(panel.background = element_blank(),
+                 panel.border = element_rect(colour = "black", fill=NA, size=1))
+Q4 = Q3 + theme(axis.text.y   = element_text(size=12))
+Q5 = Q4 + theme(axis.title.x   = element_text(size=12))
+Q5
+
+
+
+# QPA Anolis --------------------------------------------------------------
+
+AnolisQPA = read_excel("Graph _Source contribution.xlsx",19)
+AnolisQPA
+
+AnolisQPA$Data <- factor(AnolisQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
+
+S = ggplot(data=AnolisQPA, aes(x=Source, y=Value10A, ymin=LowerCI, ymax=UpperCI, color=Data))
+S = S + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
+S = S + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
+S = S + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
+S = S + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
+S = S + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
+S = S + coord_flip() + theme(legend.position="none")
+S = S + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
+S
+S1 = S +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
+                axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) # #subaxis y
+S2 = S1 +  theme(strip.text.y = element_text(face="bold", size=10, colour="black")) # Each title white
+S3 = S2 +  theme(panel.background = element_blank(),
+                 panel.border = element_rect(colour = "black", fill=NA, size=1))
+S4 = S3 + theme(axis.text.y   = element_text(size=12))
+S5 = S4 + theme(axis.title.x   = element_text(size=12))
+S5
+
+
+# QPA Leucauge ------------------------------------------------------------
+
+
+LeucaugeQPA = read_excel("Graph _Source contribution.xlsx",21)
+LeucaugeQPA
+
+LeucaugeQPA$Data <- factor(LeucaugeQPA$Data, levels=c("6mo pre-", "2mo post-", "9mo post-", "18mo post-"))
+
+U = ggplot(data=LeucaugeQPA, aes(x=Source, y=Value11A, ymin=LowerCI, ymax=UpperCI, color=Data))
+U = U + geom_pointrange(aes(col=Source), lwd=1.5) # lwd= line thick 
+U = U + geom_hline(aes(fill=Source), yintercept =0.5, linetype=2)
+U = U + xlab("") + ylab("Contribution (97.5% Confidence Interval)")
+U = U + geom_errorbar(aes(ymin=LowerCI, ymax=UpperCI, col=Source), width=0.5, cex=1)
+U = U + facet_wrap(~Data, strip.position="left", nrow=20, scales = "free_y") 
+U = U + coord_flip() + theme(legend.position="none")
+U = U + scale_colour_manual(values = c('#00BF5F', '#C68550')) #first Biofilm, second Leaf
+U
+U1 = U +  theme(axis.text.x=element_text(angle=0, size=10, vjust=0.5,hjust=1, color="black"), #subaxis x
+                axis.text.y=element_text(angle=0, size=10, vjust=0.5, color="black")) # #subaxis y
+U2 = U1 +  theme(strip.text.y = element_text(face="bold", size=10, colour="black")) # Each title white
+U3 = U2 +  theme(panel.background = element_blank(),
+                 panel.border = element_rect(colour = "black", fill=NA, size=1))
+U4 = U3 + theme(axis.text.y   = element_text(size=12))
+U5 = U4 + theme(axis.title.x   = element_text(size=12))
+U5
 
 
 
 
+# Graph togeter -----------------------------------------------------------
 
+Figure2 <- ggarrange(A5, C5, E5, G5,I5,K5,M5,O5,Q5,S5,U5,  ncol=3, nrow=4, align = "hv",
+                     labels = c("A", "B","C","D","E","F","G","H","I","J","K"),
+                     font.label = list(size = 12, color = "black"),
+                     common.legend = TRUE, legend="top")
+Figure2
 
