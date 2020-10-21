f973e3176a9d3dd8ae36fd9d8e95e9629365df5d
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-10-21 14:45:03 -0600

    Density Plots

--- Density_Plots.R
+++ Density_Plots.R
@@ -9,12 +9,15 @@
 #
 
 library(ggplot2)
+library(ggpubr)
 QPA_G_Feb17 <- read.csv("DensityPlots/QPA_Glossosomatidae_Feb17.csv")
 head(QPA_G_Feb17)
 
+
 g1 <- ggplot(QPA_G_Feb17, aes(x=densty, color=source)) + 
   labs(y="Densty", x="Sorce contribution") +
-  geom_density(size=1) +
-  scale_color_manual(values=c("#238443", "#225ea8", "#8c2d04")) +
+  geom_density(aes(linetype = source), size=1.2) +
+  scale_color_manual(values=c("#31a354", "#2c7fb8", "#d95f0e")) +
+  scale_linetype_manual(values = c("solid", "dotted", "longdash")) +
   theme_classic()
 g1
