66b115b8c9720d84948d9d898d231d4529dccc12
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-10-21 14:55:35 -0600

    Graph

--- Density_Plots.R
+++ Density_Plots.R
@@ -20,6 +20,7 @@ g1 <- ggplot(QPA_G_Feb17, aes(x=densty, color=source)) +
   scale_color_manual(values=c("#31a354", "#2c7fb8", "#d95f0e")) +
   scale_linetype_manual(values = c("solid", "dotted", "longdash")) +
   theme_classic()+
+  ylim(0, 5)+
   theme(axis.text.y=element_text(angle=0, size=12, vjust=0.5, color="black")) + #subaxis y
   theme(axis.text.x =element_text(angle=0, size=12, vjust=0.5, color="black")) + #subaxis x
   theme(axis.title.x = element_text(color="black", size=14))+ # #Axis y
